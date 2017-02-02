7ecda0992e69e59bd8e541f35d64e29c1935b752
Matt <Matt@192.168.1.37>
2017-02-02 01:10:39 +0100

    Small Correction

--- src/com/engine/Engine.java
+++ src/com/engine/Engine.java
@@ -5,15 +5,14 @@ import javax.swing.SwingUtilities;
 import com.ui.BDOWindow;
 
 public class Engine {
-	
+
 	public static void main(String[] args) {
 		SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-            	new BDOWindow();
-            }
-        });
-		
-		
+			public void run() {
+				new BDOWindow();
+			}
+		});
+
 	}
 
 }
